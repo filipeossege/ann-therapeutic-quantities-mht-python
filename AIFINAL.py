fbfa62af3cbd0345697309b2e5ac9b83906b6063
filipeossege <48655670+filipeossege@users.noreply.github.com>
2025-02-01 14:58:21 -0300

    AIFINAL.py

--- AIFINAL.py
+++ AIFINAL.py
@@ -1,25 +1,38 @@
+#Imported packages
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+
+# To install the scikit-learn (sklearn) package in the Spyder software, use the following commands
+# First install the miniconda anaconda distribution in your machine
+# Open the anaconda prompt from miniconda and create an environment with the command
+# "conda create -n spyder-env -y"
+# Activate the environment with
+# "conda activate spyder-env"
+# Then install the scikit-learn package
+# "pip install scikit-learn"
+# Then, open the Spyder software. Click in "Tools" => "Preferences" => "Python interpreter"
+# Click on "use the following Python interpreter" and choose ""spyder-env/python.ex"
+# Click on apply. Then, click in "restart kernel" in the options button on the right corner
+# The package can now be used
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPRegressor
-import matplotlib.pyplot as plt
-from scipy.interpolate import griddata
-#Parameters
-Runs = 30
-kfolds = 5
 
-#Dataset
-A = np.loadtxt("DS.txt") # (fi, H_0 [A/m], f [Hz], a [m]) and  (Tc(°C), t(s))
-X = A[:,0:4]
-Y = A[:,4:6]
+# Dataset
+A = np.loadtxt("example.txt") # Varibles in columns (fi, H_0 [A/m], f [Hz], a [m]) and  (Tc[°C], t[s])
+X = A[:,0:4] #fi, H_0 [A/m], f [Hz], a [m]
+Y = A[:,4:6] #Tc[°C], t[s]
 
+# Division in test data
 XTrain, XTest, YTrain, YTest = train_test_split(X, Y, test_size=0.2)
 YTrain1 = YTrain[:,0]
 YTrain2 = YTrain[:,1]
 YTest1 = YTest[:,0]
 YTest2 = YTest[:,1]
 
-#Standardize
+# Standardize
 scaler = StandardScaler()
 XTrain_scaled = scaler.fit_transform(XTrain)
 XTest_scaled = scaler.transform(XTest)
@@ -28,6 +41,7 @@ YTrain1_scaled = scaler_Y1.fit_transform(YTrain1.reshape(-1, 1)).ravel()
 scaler_Y2 = StandardScaler()
 YTrain2_scaled = scaler_Y2.fit_transform(YTrain2.reshape(-1, 1)).ravel()
 
+# First model
 Mdl1 = MLPRegressor(hidden_layer_sizes=(161, 10, 300),
                     activation='logistic', #logistic = sigmoid
                     alpha=5.0754e-06,
@@ -35,10 +49,11 @@ Mdl1 = MLPRegressor(hidden_layer_sizes=(161, 10, 300),
                     solver='lbfgs')
 Mdl1.fit(XTrain_scaled, YTrain1_scaled)
 
+# Second model
 Mdl2 = MLPRegressor(hidden_layer_sizes=(10, 293),
                     activation='logistic', #logistic = sigmoid
                     alpha=2.1568e-08,
-                    max_iter=10000,
+                    max_iter=1000,
                     solver='lbfgs')
 Mdl2.fit(XTrain_scaled, YTrain1_scaled)
 
@@ -63,7 +78,7 @@ plt.savefig('TIME.png', format='png', dpi=1200, bbox_inches='tight')
 R1 = np.concatenate((XTest, YTest1.reshape(-1, 1), testPredictions1.reshape(-1, 1)), axis=1)
 R2 = np.concatenate((XTest, YTest2.reshape(-1, 1), testPredictions2.reshape(-1, 1)), axis=1)
 
-# Temperature Surfaces
+# First temperature surface
 xlin = np.linspace(np.min(R1[:,0]), np.max(R1[:,0]), 50)
 ylin = np.linspace(np.min(R1[:,1]), np.max(R1[:,1]), 50)
 X1, Y1 = np.meshgrid(xlin, ylin)
@@ -81,6 +96,7 @@ ax.set_zlabel(r'$T_c$ (°C)')
 ax.view_init(elev=20, azim=70, roll=0)
 plt.savefig('1.png', format='png', dpi=1200, bbox_inches='tight')
 
+# Second temperature surface
 xlin = np.linspace(np.min(R1[:,2]), np.max(R1[:,2]), 50)
 ylin = np.linspace(np.min(R1[:,3]), np.max(R1[:,3]), 50)
 X1, Y1 = np.meshgrid(xlin, ylin)
@@ -99,7 +115,7 @@ ax.set_zlabel(r'$T_c$ (°C)')
 ax.view_init(elev=20, azim=20, roll=0)
 plt.savefig('2.png', format='png', dpi=1200, bbox_inches='tight')
 
-# Time surfaces
+# First time surface
 xlin = np.linspace(np.min(R1[:,0]), np.max(R1[:,0]), 50)
 ylin = np.linspace(np.min(R1[:,1]), np.max(R1[:,1]), 50)
 X1, Y1 = np.meshgrid(xlin, ylin)
@@ -117,6 +133,7 @@ ax.set_zlabel(r'$t_\infty$ (s)')
 ax.view_init(elev=20, azim=70, roll=0)
 plt.savefig('3.png', format='png', dpi=1200, bbox_inches='tight')
 
+# Second time surface
 xlin = np.linspace(np.min(R1[:,2]), np.max(R1[:,2]), 50)
 ylin = np.linspace(np.min(R1[:,3]), np.max(R1[:,3]), 50)
 X1, Y1 = np.meshgrid(xlin, ylin)
